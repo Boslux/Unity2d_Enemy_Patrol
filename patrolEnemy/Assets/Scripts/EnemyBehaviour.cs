e1b015214cc7552f8fddc1aa31e6427c842becf8
Tunahan <87334276+Boslux@users.noreply.github.com>
2022-12-14 18:55:21 +0300

    2d_Enemy_Patrol

---
+++ patrolEnemy/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float speed=1.5f;
+    Rigidbody2D rb;
+    BoxCollider2D bc;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (IsFaceingRight()==true)
+        {
+            //move right
+            rb.velocity = new Vector2(speed, 0);
+        }
+        
+        else
+        {
+            //move left
+            rb.velocity = new Vector2(-speed, 0);
+           
+        }
+    }
+    void OnTriggerExit2D(Collider2D cls)
+    {
+        // for turn
+        transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)),transform.localScale.y);
+    }
+    private bool IsFaceingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+}
